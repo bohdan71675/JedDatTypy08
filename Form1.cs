00b5d9fdd38755c6296d8a72dc0102f33fb62cb7
bodnarb.04 <bodnarb.04@gmail.com>
2022-12-09 08:40:26 +0100

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JedDatTypy08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static bool JePrvocislo(int x)
+        {
+            bool prvocilo = true;
+
+            if (x == 0)
+            {
+                prvocilo = false;
+            }
+            else if (x >= 3)
+            {
+                for (int i = 2; i <= x / 2 && prvocilo; i++)
+                {
+                    if (x % i == 0)
+                    {
+                        prvocilo = false;
+                    }
+                }
+            }
+            return prvocilo;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < textBox1.Lines.Count(); i++)
+            {
+                int cislo = int.Parse(textBox1.Lines[i].ToString());
+                if (JePrvocislo(cislo))
+                {
+                    MessageBox.Show("Obsahuje prvocislo");
+                }
+                else
+                {
+                    MessageBox.Show("Neobsahuje prvocislo");
+                }
+            }
+
+
+
+        }
+    }
+}
